1e5fd727003228b5c736564663e08f92109a70ff
iamtaehoon <kimth9981@naver.com>
2022-02-15 08:43:07 +0900

    feat(Category): 카테고리에 이름을 붙여 보여주는 기능 구현

--- src/main/java/young/board/domain/Category.java
+++ src/main/java/young/board/domain/Category.java
@@ -1,5 +1,14 @@
 package young.board.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-    QUESTION, CHAT, NOTICE; //질문글, 잡담글, 공지사항
+    QUESTION("질문"), CHAT("잡담"), NOTICE("공지사항"); //질문, 잡담, 공지사항
+
+    private String category;
+
+    Category(String category) {
+        this.category = category;
+    }
 }
