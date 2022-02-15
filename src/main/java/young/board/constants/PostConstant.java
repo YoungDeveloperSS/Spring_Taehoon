159ba1f171690c2877493df78bcda3c07f72a0c9
iamtaehoon <kimth9981@naver.com>
2022-02-15 09:25:37 +0900

    fix(): PostForm의 @size의 min,max 변수 컴파일오류 발생 이슈 해결

--- src/main/java/young/board/constants/PostConstant.java
+++ src/main/java/young/board/constants/PostConstant.java
@@ -1,10 +1,10 @@
 package young.board.constants;
 
 public abstract class PostConstant {
-    public static final Integer TITLE_MIN_LENGTH = 1;
-    public static final Integer TITLE_MAX_LENGTH = 20;
-    public static final Integer WRITER_MIN_LENGTH = 1;
-    public static final Integer WRITER_MAX_LENGTH = 8;
+    public static final int TITLE_MIN_LENGTH = 1;
+    public static final int TITLE_MAX_LENGTH = 20;
+    public static final int WRITER_MIN_LENGTH = 1;
+    public static final int WRITER_MAX_LENGTH = 8;
 
     public static final Integer DEFAULT_LIKE_NUM = 0;
 
