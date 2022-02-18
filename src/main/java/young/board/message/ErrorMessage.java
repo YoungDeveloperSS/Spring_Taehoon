fadc411e29e0948236fedbabe497da040722ba88
iamtaehoon <kimth9981@naver.com>
2022-02-18 18:18:39 +0900

    fix(): 사용자가 없는데 게시물이 없다고 나오는 이슈 해결

--- src/main/java/young/board/message/ErrorMessage.java
+++ src/main/java/young/board/message/ErrorMessage.java
@@ -3,7 +3,7 @@ package young.board.message;
 public abstract class ErrorMessage {
     public static final String PARAM_FORM_ERROR = "파라미터가 제대로 입력되지 않았습니다.";
     public static final String NOT_EXIST_POST_ERROR = "삭제되었거나 존재하지 않는 게시글입니다.";
-    public static final String NOT_EXIST_USER_ERROR = "삭제되었거나 존재하지 않는 게시글입니다.";
+    public static final String NOT_EXIST_USER_ERROR = "삭제되었거나 존재하지 않는 사용자입니다.";
 
     public static final String TITLE_NOT_INPUT_ERROR = "제목을 입력해주세요.";
     public static final String TITLE_LENGTH_ERROR = "제목은 1글자 이상 20글자 이하여야 합니다.";
