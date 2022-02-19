972a8e8d55b6d94fdbfe2a97c572c1e033eaa187
iamtaehoon <kimth9981@naver.com>
2022-02-19 16:53:36 +0900

    refactor(PostController): RestController로 변경

--- src/main/java/young/board/controller/PostController.java
+++ src/main/java/young/board/controller/PostController.java
@@ -1,8 +1,9 @@
 package young.board.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import young.board.controller.form.PostCreateForm;
 import young.board.controller.form.PostEditForm;
@@ -18,7 +19,7 @@ import java.util.stream.Collectors;
 import static young.board.constants.PostConstant.*;
 import static young.board.message.ErrorMessage.PARAM_FORM_ERROR;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
@@ -26,93 +27,87 @@ public class PostController {
     private final RecommendationService recommendationService;
 
     @GetMapping
-    public String showPostList(Model model) {
+    @ResponseStatus(HttpStatus.OK) //OK말고 다른 상황은 없으니까
+    public List<PostResponse> showPostList() {
         List<Post> posts = postService.findAll();
         List<PostResponse> postResponses = posts.stream().map(post ->
                         PostResponse.create(post, recommendationService.calculateLikesCnt(post.getId())))
                 .collect(Collectors.toList());
-        model.addAttribute("postResponses", postResponses);
-        return "post-list";
+        return postResponses;
     }
 
     @GetMapping("/{id}")
-    public String showPostDetail(@PathVariable Long id, Model model) {
+    public ResponseEntity<PostResponse> showPostDetail(@PathVariable Long id) {
         try {
             Post post = postService.findPost(id);
-            model.addAttribute("post", post);
+            PostResponse postResponse = PostResponse.create(post, recommendationService.calculateLikesCnt(post.getId()));
+            return new ResponseEntity<>(postResponse, HttpStatus.OK);
         } catch (IllegalStateException e) {
-            model.addAttribute("error", e.getMessage());
-            return "error-page";
+            return ResponseEntity.notFound().build();
         }
-        return "post";
     }
 
     @DeleteMapping("/{id}")
-    public String deletePost(@PathVariable Long id, Model model) {
+    public ResponseEntity<String> deletePost(@PathVariable Long id) {
         try {
             postService.deletePost(id);
+            return new ResponseEntity<>("delete complete", HttpStatus.OK);
         } catch (IllegalStateException e) {
-            model.addAttribute("error", e.getMessage());
-            return "error-page";
+            return ResponseEntity.notFound().build();
         }
-        return "redirect:/posts";
     }
 
     @GetMapping("/{id}/edit")
-    public String editPostForm(@PathVariable Long id, Model model) {
+    public ResponseEntity<PostEditForm> editPostForm(@PathVariable Long id) {
         try {
             Post post = postService.findPost(id);
             PostEditForm form = PostEditForm.createPostEditForm(post);
-            model.addAttribute("form", form);
+            return new ResponseEntity<>(form, HttpStatus.OK);
         } catch (IllegalStateException e) {
-            model.addAttribute("error", e.getMessage());
-            return "error-page";
+            return ResponseEntity.notFound().build();
         }
-        return "post-edit-form";
     }
 
-    @PostMapping("/{id}")
-    public String editPost(@PathVariable Long id, @ModelAttribute PostEditForm form, Model model) {
+    @PatchMapping("/{id}")
+    public ResponseEntity<String> editPost(@PathVariable Long id, @ModelAttribute PostEditForm form) {
         //TODO 검증 -> View에서도 검증 해줘야함.
         try {
             validatePostForm(form.getTitle(), form.getWriter(), form.getContent(), form.getCategory());
             postService.update(id, form.getTitle(), form.getWriter(),
                     form.getContent(), form.getCategory());
+            //이런 경우 id값이 필요한거같은데, id 넘겨야 하나?
+            return new ResponseEntity<>("update complete", HttpStatus.OK);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return "redirect:/";
+            return ResponseEntity.badRequest().build();
         } catch (IllegalStateException e) {
-            model.addAttribute("error", e.getMessage());
-            return "error-page";
+            return ResponseEntity.notFound().build();
         }
-        return "redirect:/posts/" + id;
+//        return "redirect:/posts/" + id;
     }
 
     @GetMapping("/new")
-    public String createPostForm(Model model) {
-        model.addAttribute("form", new PostCreateForm());
-        return "post-create-form";
+    @ResponseStatus(HttpStatus.OK)
+    public PostCreateForm createPostForm() {
+        return new PostCreateForm();
     }
 
     @PostMapping
-    public String createPost(@ModelAttribute PostCreateForm form, Model model) {
+    public ResponseEntity<String> createPost(@ModelAttribute PostCreateForm form) {
         //TODO 검증 -> View에서도 검증 해줘야함.
         try {
             validatePostForm(form.getTitle(), form.getWriter(), form.getContent(), form.getCategory());
             Long savedId = postService.save(form.getTitle(), form.getWriter(), form.getContent(), form.getCategory());
-            return "redirect:/posts/" + savedId;
+            return new ResponseEntity<>("create complete", HttpStatus.OK);
+            //이런 경우 id값이 필요한거같은데, id 넘겨야 하나?
         } catch (IllegalArgumentException e) {
-            model.addAttribute("error", e.getMessage());
-            return "error-page";
+            return ResponseEntity.badRequest().build();
         } catch (IllegalStateException e) {
-            model.addAttribute("error", e.getMessage());
-            return "error-page";
+            return ResponseEntity.notFound().build();
         }
-        //TODO 왜 여기 리턴 없어도 돌아가는건지 공부해보기.
     }
 
     private void validatePostForm(String title, String writer, String content, Category category) {
-        if (title.isBlank() || writer.isBlank() ||
+        if (!StringUtils.hasText(title) || !StringUtils.hasText(writer) ||
                 content == null || category == null ||
                 title.length() < TITLE_MIN_LENGTH || title.length() > TITLE_MAX_LENGTH ||
                 writer.length() < WRITER_MIN_LENGTH || writer.length() > WRITER_MAX_LENGTH) {
